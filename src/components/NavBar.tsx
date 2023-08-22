7656c37ad65f6f98f138cfbcf9a2841a2d880f79
mcherni <cherni.moemen@gmail.com>
2023-08-22 21:16:43 +0200

    Add global theme and the color mode switch component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import { HStack, Text } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
       <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
